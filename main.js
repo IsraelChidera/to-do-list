9128ba6525ee99be89d6fc345e415fe98cbfae91
israelchidera <israelchidera54@gmail.com>
2020-07-23 20:53:18 +0100

    Styling upload

--- main.js
+++ main.js
@@ -3,19 +3,23 @@ let li = document.querySelector("li");
 let inputValue = document.querySelector("input[type=text]");
 let lis = document.querySelectorAll("li");
 let icons = document.querySelectorAll(".deleteicon");
+let plusButton = document.querySelector(".fa-plus");
 
+// loop through list and apply event for todo cancellation
 for(let i=0; i<lis.length; i++){
     lis[i].addEventListener("click", function(){
         this.classList.toggle("completed");
     })
 }
 
+//loop through list to delete done todos
 for(let i=0; i<icons.length; i++){
     icons[i].addEventListener("click", function(){
         this.parentElement.remove();
     })
 }
 
+//add event listener to input
 inputValue.addEventListener("keyup", function(event){
     if(event.key === "Enter") {
         let newLi = document.createElement("li");
@@ -24,6 +28,7 @@ inputValue.addEventListener("keyup", function(event){
         let icon = document.createElement("i");
         icon.classList.add("fa", "fa-trash-alt");
 
+        //append to form new lists 
         span.appendChild(icon);
         newLi.appendChild(text);
         newLi.appendChild(span);
@@ -38,4 +43,28 @@ inputValue.addEventListener("keyup", function(event){
         inputValue.value = "";
     }
     
+})
+
+plusButton.addEventListener("click", function(){
+    if(true) {
+        let newLi = document.createElement("li");
+        let text = document.createTextNode(inputValue.value);
+        let span = document.createElement("span");
+        let icon = document.createElement("i");
+        icon.classList.add("fa", "fa-trash-alt");
+
+        //append to form new lists 
+        span.appendChild(icon);
+        newLi.appendChild(text);
+        newLi.appendChild(span);
+
+        ul.appendChild(newLi);
+        newLi.addEventListener("click", function(){
+            this.classList.toggle("completed");
+        })
+        span.addEventListener("click", function(){
+            this.parentElement.remove();
+        })
+        inputValue.value = "";
+    }
 })
