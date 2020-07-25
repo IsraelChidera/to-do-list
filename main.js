e18bc7c2b370353000e5e474b4e9cb8065125d23
israelchidera <israelchidera54@gmail.com>
2020-07-25 03:11:12 +0100

    added a dynamic background and a time

--- main.js
+++ main.js
@@ -68,3 +68,37 @@ plusButton.addEventListener("click", function(){
         inputValue.value = "";
     }
 })
+
+function updateTime() {
+    let today = new Date();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+
+
+
+    let currentTime = hours + ' : ' + minutes + ' : ' + seconds + " WAT ";
+    
+    let now = document.querySelector("#clock");
+    now.innerHTML = currentTime;
+    now.style.color= "#f11712";
+    now.style.fontSize = "20px";
+    setTimeout(updateTime, 500);
+}
+
+let i = 0;
+const imageLinks = [
+    "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+     "https://images.unsplash.com/photo-1535357902142-d1baa8375bbc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1508710285745-edc8137d6b5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+];
+
+function change() {
+   if(i >= imageLinks.length){
+    i = 0;
+  }
+  document.getElementById('bg').style.backgroundImage = "url('"+ imageLinks[i++] + "')";
+ 
+}  
+
+setInterval(change, 5000);
